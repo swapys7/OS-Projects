f8b726cf85d2c62535e20a9fce292a7058135b4d
Swapnil Joshi <swapnil.joshi777@gmail.com>
2014-03-17 16:44:35 -0700

    Revert "Personal HTTP Web Server"
    This reverts commit 775d4ddd52e54eed46a77e07f2b7880f2f3e0fad.

--- HTTP Web Server Threadpool and signalling/header.h
+++
@@ -1,9 +0,0 @@
-#ifndef _header_H
-#define _header_H
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-char *get_file_type(const char *);
-#endif
--- HTTP Web Server Threadpool and signalling/print_log.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CSUC_HTTP_SWAPNILJOSHI_H
-#define _CSUC_HTTP_SWAPNILJOSHI_H
-
-#include <stdio.h>
-#include <stdlib.h>
-
-void print_log_level_info();
-
-#endif
